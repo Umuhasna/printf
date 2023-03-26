0e1cc539920ae4e5e226216b1304ddaf17dae53f
Umuhasna <fa1033170@gmail.com>
2023-03-26 08:03:27 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 int _printf(const char *format, ...);
 
 #endif
